427cd55954898914d595519f029672adf5e09a50
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-10-19 00:44:30 +0100

    updated

--- DSA-c#/Lists_DSA/ConsoleApp1/Program.cs
+++ DSA-c#/Lists_DSA/ConsoleApp1/Program.cs
@@ -1,24 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1;
 using StackLibrary;
+using System.Collections;
 
+/*
 var myList = new CustomLinkedList2<int>();
 var myStack = new Stack<int>();
 var myStack1 = new CustomStack<int>();
 var myQueue = new Queue<int>();
 var myQueue1 = new CustomQueue<int>();
+*/
+
+//var node1 = new Node2<int>(1);
+//var node2 = new Node2<int>(2);
+//var node3 = new Node2<int>(3);
 
 
-var node1 = new Node2<int>(1);
-var node2 = new Node2<int>(2);
-var node3 = new Node2<int>(3);
-
-
-myStack1.Push(node1);
-myStack1.Push(node2);
-myStack1.Push(node3);
-myStack1.Pop();
-myStack1.Peek();
+//myStack1.Push(node1);
+//myStack1.Push(node2);
+//myStack1.Push(node3);
+//myStack1.Pop();
+//myStack1.Peek();
 
 //myQueue1.Enqueue(node1);
 //myQueue1.Enqueue(node2);
@@ -26,11 +29,11 @@ myStack1.Peek();
 //myQueue1.Dequeue();
 
 
-myStack.Push(1);
-myStack.Push(2);
-myStack.Push(3);
-myStack.Pop();
-myStack.Peek();
+//myStack.Push(1);
+//myStack.Push(2);
+//myStack.Push(3);
+//myStack.Pop();
+//myStack.Peek();
 
 //var node1 = new Node2<string>("Hello");
 //var node2 = new Node2<string>("Please");
@@ -51,6 +54,30 @@ myStack.Peek();
 //myList.DeleteNode(node2);
 
 //Console.WriteLine(myList.ToString());
-Console.WriteLine(myStack.ToString());
+//Console.WriteLine(myStack.ToString());
 //Console.WriteLine(myStack1.ToString());
+
+var myDict = new Dictionary<int, Student>();
+var myHash = new Hashtable();
+var listOfStudents = new List<Student>()
+{
+    new Student("Gideon", "Bayelsa", 8),
+    new Student("Timothy", "Rivers", 3)
+};
+
+foreach (var student in listOfStudents)
+{
+    myDict.Add(student.Level, student);
+}
+
+foreach (var kvp in myDict)
+{
+    Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+}
+
+
+Console.WriteLine();
+
+
+
 
---
+++ DSA-c#/Lists_DSA/ConsoleApp1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public Student(string name, string address, int level)
+        {
+            Name = name;
+            Address = address;
+            Level = level;
+        }
+
+        public override string ToString()
+        {
+            var res = Name + " " + Address;
+            return res;
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Level { get; set; }
+    }
+}
